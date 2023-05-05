b782c5fbe86dfec7d43e3cf6fc00b37b49f09cb7
H. Kodama <heckodama@hotmail.com>
2023-05-05 00:12:07 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.href='#esquerdaMain'
+}
+
+function galeria() {
+    window.location.href='#galeriaSection'
+}
+
+function elementos() {
+    window.location.href='#elementosSection'
+}
